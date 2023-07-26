2163e2fdd7fd7fb515fcc55476735db7053809a6
Pain <asskarnwin@gmail.com>
2023-07-26 22:45:38 +0700

    add claim dividend

--- programs/ft-trading/src/intructions/claim.rs
+++ programs/ft-trading/src/intructions/claim.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::TokenAccount;
 
-use crate::{UserShareAccount, START_TS, EPOCH_DURATION, DividendVault, WhitelistedNFT};
+use crate::{UserShareAccount, START_TS, EPOCH_DURATION, DividendVault, WhitelistedNFT, instruction, DividendVaultWallet};
 use crate::DummyError;
 
 #[derive(Accounts)]
@@ -137,6 +137,102 @@ pub fn claim_share(
     user_share_account.reward_share += reward_share_received;
     dividend_vault.total_share += reward_share_received;
     whitelisted_nft.last_claim_ts = now_ts;
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+#[instruction(
+    epoch: u64,
+    user_share_account_bump: u8,
+    dividend_vault_bump: u8,
+    dividend_vault_wallet_bump: u8,
+)]
+pub struct ClaimDividend<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"user_share_account",
+            epoch.to_le_bytes().as_ref(),
+            owner.key().as_ref()
+        ],
+        bump = user_share_account_bump,
+        close = owner
+    )]
+    pub user_share_account: Account<'info, UserShareAccount>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [
+            b"dividend_vault",
+            epoch.to_le_bytes().as_ref()
+        ],
+        bump=dividend_vault_bump,
+    )]
+    pub dividend_vault: Account<'info, DividendVault>,
+    #[account(
+        mut,
+        seeds = [
+            b"dividend_vault_waller",
+            dividend_vault.key().as_ref()
+        ],
+        bump= dividend_vault_wallet_bump
+    )]
+    pub dividend_vault_wallet: Account<'info, DividendVaultWallet>,
+}
+
+pub fn claim_dividend(
+    ctx: Context<ClaimDividend>,
+    epoch: u64,
+    _user_share_account_bump: u8,
+    _dividend_vault_bump: u8,
+    _dividend_vault_wallet_bump: u8
+) -> Result<()> {
+    let user_share_account = &mut ctx.accounts.user_share_account;
+    let owner = &mut ctx.accounts.owner;
+    let dividend_vault = &ctx.accounts.dividend_vault;
+    let dividend_vault_wallet = &mut ctx.accounts.dividend_vault_wallet;
+
+    if user_share_account.owner != owner.key() {
+        msg!("user_share_account.owner != owner.key()");
+        return err!(DummyError::Error);
+    }
+
+    if user_share_account.epoch != epoch {
+        msg!("user_share_account.epoch != epoch");
+        return err!(DummyError::Error);
+    }
+
+    if user_share_account.epoch != dividend_vault.epoch {
+        msg!("user_share_account.epoch != dividend_vault.epoch");
+        return err!(DummyError::Error);
+    }
+
+    let now_ts = Clock::get().unwrap().unix_timestamp;
+    let current_epoch = ((now_ts - START_TS)/EPOCH_DURATION) as u64;
+
+    if user_share_account.epoch >= current_epoch {
+        msg!("can't claim now");
+        return err!(DummyError::Error);
+    }
+
+    let reward_amount = 
+    ((user_share_account.reward_share as f64) 
+    / (dividend_vault.total_share as f64) 
+    * (dividend_vault.lamport_dividend_amount as f64)) as u64;
+
+    let src = &mut dividend_vault_wallet.to_account_info();
+    **src.try_borrow_mut_lamports()? = src
+    .lamports()
+    .checked_sub(reward_amount)
+    .ok_or(ProgramError::InvalidArgument)?;
+
+    let dst = &mut owner.to_account_info();
+    **dst.try_borrow_mut_lamports()? = dst
+    .lamports()
+    .checked_add(reward_amount)
+    .ok_or(ProgramError::InvalidArgument)?;
 
     Ok(())
 }
