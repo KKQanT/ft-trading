7414d0ccd6da5903b05c405795f505137de86d0a
Pain <asskarnwin@gmail.com>
2023-08-27 19:55:00 +0700

    config epoch duration

--- programs/ft-trading/src/constant.rs
+++ programs/ft-trading/src/constant.rs
@@ -1,4 +1,4 @@
 pub const SUPER_AUTHORITY: &'static str = "SUPsBGEfUwXkwcpXFb3E265xf8RNGFuw3PD8ejbzTBC";
-pub const START_TS:i64 = 1691587000 - 86400*3;
-pub const EPOCH_DURATION:i64 = 86400; 
+pub const START_TS:i64 = 1691587000 - (86400 * 30) * 20;
+pub const EPOCH_DURATION:i64 = 86400 * 30; 
 pub const PLATFORM_FEE: u8 = 10;
