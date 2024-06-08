6aaa2529d5d956eef8ee58769db51a2995eb0e06
KKQanT <asskarnwin@gmail.com>
2024-06-08 14:17:47 +0100

    change super auth

--- programs/ft-trading/src/constant.rs
+++ programs/ft-trading/src/constant.rs
@@ -1,4 +1,4 @@
-pub const SUPER_AUTHORITY: &'static str = "";
+pub const SUPER_AUTHORITY: &'static str = "ALjJ8rxpGjAta9NA1iyhvEFUrzKjGCetcQr1rqFnjFw1";
 pub const START_TS:i64 = 1691587000 - (86400 * 30) * 20;
 pub const EPOCH_DURATION:i64 = 86400 * 30; 
 pub const PLATFORM_FEE: u8 = 10;
